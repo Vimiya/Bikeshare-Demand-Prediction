ae67543c3526f6c35eb0a0ce2c77dbea23d083fa
Vimiya <vimiya22@gmail.com>
2023-09-11 23:20:22 +0530

    Data Transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,116 @@
+import sys
+from dataclasses import dataclass
+
+import numpy as np 
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder,StandardScaler
+
+from src.exception import CustomException
+from src.logger import logging
+import os
+
+from src.utils import save_object
+# , evaluate_models
+
+@dataclass
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts',"preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transformer_object(self):
+        '''
+        This function is responsible for data transformation
+        
+        '''
+        try:
+            numerical_columns = ["temp", "hum","windspeed"]
+            categorical_columns = ["season","yr","mnth","hr","holiday", "weekday","workingday","weathersit"]
+            num_pipeline= Pipeline(
+                steps=[
+                ("scaler",StandardScaler())
+                ]
+            )
+
+            cat_pipeline=Pipeline(
+
+                steps=[
+                ("one_hot_encoder",OneHotEncoder(handle_unknown = 'error',drop='first',sparse=False)),
+                # ("one_hot_encoder",OneHotEncoder(handle_unknown = 'ignore')),
+                ("scaler",StandardScaler(with_mean=False))
+                ]
+
+            )
+
+            logging.info(f"Categorical columns: {categorical_columns}")
+            logging.info(f"Numerical columns: {numerical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                ("num_pipeline",num_pipeline,numerical_columns),
+                ("cat_pipelines",cat_pipeline,categorical_columns)
+
+                ],remainder="passthrough"
+            )
+
+            return preprocessor
+        
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+    def initiate_data_transformation(self,train_path,test_path):
+
+        try:
+            train_df=pd.read_csv(train_path)
+            test_df=pd.read_csv(test_path)
+
+            logging.info("Read train and test data completed")
+
+            logging.info("Obtaining preprocessing object")
+
+            preprocessing_obj=self.get_data_transformer_object()
+
+            target_column_name="cnt"
+
+
+            input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
+            target_feature_train_df=train_df[target_column_name]  
+            # target_feature_train_df=np.array(target_feature_train_df).reshape(-1,1) # to dim 2
+
+            input_feature_test_df=test_df.drop(columns=[target_column_name],axis=1)
+            target_feature_test_df=test_df[target_column_name]
+            # target_feature_test_df=np.array(target_feature_test_df).reshape(-1,1) # to dim 2
+            
+            logging.info(
+                f"Applying preprocessing object on training dataframe and testing dataframe."
+            )
+
+            input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
+            input_feature_test_arr=preprocessing_obj.transform(input_feature_test_df)
+
+            # train_arr = np.column_stack((input_feature_train_arr.toarray(),target_feature_train_df.to_numpy()))
+            # test_arr = np.column_stack((input_feature_test_arr.toarray(),target_feature_test_df.to_numpy()))
+
+            train_arr = np.c_[input_feature_train_arr, np.array(target_feature_train_df)]
+            test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
+
+            logging.info(f"Saved preprocessing object.")
+
+            save_object(
+ 
+                file_path=self.data_transformation_config.preprocessor_obj_file_path,
+                obj=preprocessing_obj
+
+            )
+
+            return (
+                train_arr,
+                test_arr,
+                self.data_transformation_config.preprocessor_obj_file_path,
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
